d2d3b77b8ffd5e363ce19199953e7f4b01ac3086
isabellechoi11 <72999989+isabellechoi11@users.noreply.github.com>
2022-03-01 21:37:39 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import "materialize-css/dist/css/materialize.min.css"
 import './App.css';
 import ProfilePage from './components/ProfilePage';
-import SignUp from './components/SignUp';
-import SignIn from './components/SignIn';
 import Navbar from './components/Navbar';
 import SingleReview from './components/SingleReview'
 
