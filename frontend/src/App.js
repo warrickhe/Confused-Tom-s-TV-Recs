6f887a7a181339576ff94f82db2947ace8731bc2
isabellechoi11 <72999989+isabellechoi11@users.noreply.github.com>
2022-03-01 22:20:03 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import "materialize-css/dist/css/materialize.min.css"
 import './App.css';
 import ProfilePage from './components/ProfilePage';
 import Navbar from './components/Navbar';
-import SingleReview from './components/SingleReview'
+import SingleReview from './components/SingleReview';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
 
 function App() {
