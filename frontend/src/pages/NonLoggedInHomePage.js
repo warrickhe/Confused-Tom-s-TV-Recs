e97a6cfb3b25e0cab99e93092e3822d887bad307
Marknima <nimaveravit@gmail.com>
2022-03-10 09:00:22 -0800

    Homepage Updated

--- frontend/src/pages/NonLoggedInHomePage.js
+++ frontend/src/pages/NonLoggedInHomePage.js
@@ -7,46 +7,46 @@ import { Link } from "react-router-dom";
 function NonLoggedInPage() {
   return (
     <div
-    style={{
-      display:"flex",
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems:'center'
-    }}>
-      <div
       style={{
-        width:100,
-        height:100,
-        borderRadius: 100/2,
-        overflow: "hidden"
-      }}/>
-      <div
-      style={{
-        width:250,
-        height:250,
-        borderRadius: 100/2,
-        overflow: "hidden"
+        display: "flex",
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
       }}>
+      <div
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 100 / 2,
+          overflow: "hidden"
+        }} />
+      <div
+        style={{
+          width: 250,
+          height: 250,
+          borderRadius: 100 / 2,
+          overflow: "hidden"
+        }}>
         <img
           src={F2}
           alt="logo"
           height="100%"
 
-          
+
         />
       </div>
 
+      <div className="homewindow">
+        <div >
+          <center><h3 style={{ color: 'paleturquoise' }}> CONFUSED TOM'S TV RECS </h3></center>
+          <center><h5>The perfect place for you and your friends to view and share reviews of many movies and TV shows. </h5></center>
+        </div>
 
-      <div className="app" >
-        <center><h3> Welcome to CONFUSED TOM'S TV RECS </h3></center>
-        <center><h5>The perfect place for you and your friends to view and share reviews of many movies and TV shows. </h5></center>
+        <center><div>
+          <Link to="/Sign-In" class='button'>Sign In</Link>
+          <Link to="/Sign-Up" class='button'>Sign Up</Link>
+        </div></center>
       </div>
-
-      <div>
-        <Link to="/Sign-In" class='button'>Sign In</Link> 
-        <Link to="/Sign-Up" class='button'>Sign Up</Link>
-      </div>
-
 
     </div>
   );
